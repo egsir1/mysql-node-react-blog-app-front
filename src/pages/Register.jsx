abcac6d95557e43d1352dda9e0eb475dcb82dc9d
egsir1 <egsir111@gmail.com>
2023-12-03 07:50:30 +0900

    BRR: publishing home & navbar & footer

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder="username" />
+        <input required type="text" placeholder="email" />
+        <input required type="password" placeholder="password" />
+        <button>Login</button>
+        <p>This is an error</p>
+        <span>
+          Already have an account <Link to={"/login"}>Login</Link>{" "}
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;

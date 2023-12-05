dd62799bce93a6e37ca18d33c8c5ee61156010b1
egsir1 <egsir111@gmail.com>
2023-12-05 12:06:50 +0900

    feat: GetAllposts and single post fetched from backend

--- src/pages/Singlex.jsx
+++ src/pages/Singlex.jsx
@@ -1,61 +1,65 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 const Singlex = () => {
+  const [post, setPost] = useState({});
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location.pathname);
+  const postId = location.pathname.split("/")[2];
+  const { currentUser } = useContext(AuthContext);
+  console.log("🚀 ~ file: Singlex.jsx:12 ~ Singlex ~ postId:", postId);
+
+  // fetch single post
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
+
+  // delete post
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
-            <span>John</span>
-            <p>Posted 2days ago</p>
+            <span>{post?.username}</span>
+            <p>Posted {moment(post?.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser?.username === post?.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          tempore possimus quasi rerum nulla ea similique fuga placeat aliquid
-          ipsum.
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum ea
-          dolores voluptatum quaerat aliquid aliquam quod sunt dolore omnis
-          blanditiis dolorum labore nam exercitationem, modi iure totam aperiam
-          libero quos harum ipsum fuga qui, ullam autem soluta? Cupiditate neque
-          amet explicabo commodi autem hic sint quis nam accusamus, ullam ipsum
-          praesentium veniam fugiat rem ut illo cumque repellendus quos
-          perferendis. Dignissimos aut, quasi sequi corrupti repudiandae, nobis
-          saepe magni exercitationem, sit itaque quae! Deleniti dolor dolore,
-          error ut vitae ipsam ab. Reprehenderit quia nesciunt doloribus vitae
-          assumenda placeat aliquid, excepturi, aliquam itaque reiciendis
-          laudantium vel. Voluptate fuga reprehenderit molestiae excepturi
-          possimus magnam non beatae hic, ipsam ut quae eaque illo et sed ipsum
-          cum! Sapiente dignissimos, dolorem inventore voluptatum, animi nobis
-          praesentium reprehenderit doloremque impedit omnis natus modi ipsam
-          hic ea officia fugit eos necessitatibus laborum cum vitae libero, qui
-          tempora. Voluptatem obcaecati sit laudantium fuga, et mollitia ab
-          laboriosam esse dolorum quae a cum, aliquid accusantium! Molestiae
-          optio nihil aut est deleniti nam ducimus voluptates praesentium
-          provident iste veritatis quisquam minus assumenda quo alias, harum
-          officia recusandae, adipisci tempora non labore odit? A soluta nobis
-          voluptate laudantium nam iure recusandae quia quisquam, omnis,
-          perspiciatis ducimus, fugiat praesentium numquam culpa.
-        </p>
+        <h1>{post?.title}</h1>
+        {post?.desc}
       </div>
       <Menu />
     </div>

30cde5ac930e5d36b818d2f80372cb8a59c6f9df
egsir1 <egsir111@gmail.com>
2023-12-05 09:29:11 +0900

    feat: User registration logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [registerUsers, setRegsiterUsers] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setRegsiterUsers((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", registerUsers);
+      navigate("/login");
+      console.log("🚀 ~ file: Register.jsx:20 ~ handleSubmit ~ res:", res);
+    } catch (err) {
+      setError(err.response.data);
+      console.log(err);
+    }
+  };
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="password" placeholder="password" />
-        <button>Login</button>
-        <p>This is an error</p>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>
           Don't you have an account <Link to={"/register"}>Register</Link>{" "}
         </span>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [registerUsers, setRegsiterUsers] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setRegsiterUsers((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", registerUsers);
+      navigate("/login");
+      console.log("🚀 ~ file: Register.jsx:20 ~ handleSubmit ~ res:", res);
+    } catch (err) {
+      setError(err.response.data);
+      console.log(err);
+    }
+  };
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="text" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Login</button>
-        <p>This is an error</p>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="text"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>
           Already have an account <Link to={"/login"}>Login</Link>{" "}
         </span>
